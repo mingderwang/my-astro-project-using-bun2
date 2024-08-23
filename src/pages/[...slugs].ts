5cd1416b349bbe88afd4d0546ac7d4d0d32bcdcd
mingderwang <mingderwang@me.com>
2024-08-23 18:05:01 +0800

    add vercel and elysia with swagger

---
+++ src/pages/[...slugs].ts
@@ -0,0 +1,29 @@
+
+// pages/[...slugs].ts
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+const app = new Elysia()
+  .get("/counter", ({ store }) => store.counter++)
+  .use(swagger())
+  .patch(
+    "/user/profile",
+    ({ body, error }) => {
+      if (body.age < 18) return error(400, "Oh no");
+
+      if (body.name === "Nagisa") return error(418);
+
+      return body;
+    },
+    {
+      body: t.Object({
+        name: t.String(),
+        age: t.Number(),
+      }),
+    }
+  )
+
+export type App = typeof app;
+
+const handle = ({ request }: { request: Request }) => app.handle(request);
+
+export const GET = handle;
